8b0684a45024dbfa15177304eb66296cd3d782f6
Milli-saved <million12.tenkir@gmail.com>
2024-06-28 14:08:54 +0300

    Minor: fix signin page..

--- src/Routes/allRoutes.jsx
+++ src/Routes/allRoutes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-import BasicSignIn from "../pages/AuthenticationInner/Login/Signin";
+// import BasicSignIn from "../pages/AuthenticationInner/Login/Signin";
 import Login from "../pages/Authentication/Login";
 
 const publicRoutes = [{ path: "/", Component: <Login /> }];
